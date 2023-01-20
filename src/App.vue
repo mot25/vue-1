6f12574dc22e58bf2aab03e19a6a483ece591dd0
mot25 <mot.112511.pog@gmail.com>
2023-01-20 12:32:25 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,129 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header>
+    <h1>Developer <strong>by</strong> Pogodaev</h1>
+    <form class="search-box" @submit.prevent="handleSearch">
+      <input type="text" class="search-field" placeholder="Search for an palce..." required v-model="searchQuery" />
+    </form>
+  </header>
+  <main>
+    <div class="cards" v-if="palceList.length > 0">
+      <CardsComp v-for="palce in palceList" :key="palce.mal_id" :palce="palce" />
+    </div>
+    <div class="no-results" v-else>Sorry, we have no results...</div>
+  </main>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import CardsComp from "./components/CardComp.vue";
+import { ref } from "vue";
 
-@Options({
+export default {
   components: {
-    HelloWorld,
+    CardsComp,
   },
-})
-export default class App extends Vue {}
-</script>
+  setup() {
+    const searchQuery = ref("");
+    const palceList = ref([]);
+    const handleSearch = async () => {
+      palceList.value = await fetch(
+        `https://nominatim.openstreetmap.org/?addressdetails=1&q=${searchQuery.value}&format=json`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+      console.log(palceList.value);
+    };
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    return {
+      searchQuery,
+      palceList,
+      handleSearch,
+    };
+  },
+};
+</script>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+      background-color: #f3f3f3;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+      color: #313131;
+      font-size: 20px;
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.15);
+      }
+    }
+  }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+  display: flex;
+  justify-content: center;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 895px;
+  }
 }
 </style>
---
+++ src/components/CardComp.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="card">
+    <h6 class="titleWarn">Тут должна было быть картинка, но мою карту API не принимает</h6>
+    <div v-if="palce?.address.state" class="wrapperText">
+      <strong>state:</strong>
+      <p>{{ palce?.address.state }}</p>
+    </div>
+    <div v-if="palce.address.state" class="wrapperText">
+      <strong>country:</strong>
+      <p>{{ palce.address.country }}</p>
+    </div>
+    <div v-if="palce.address.state" class="wrapperText">
+      <strong>state:</strong>
+      <p>{{ palce.address.state }}</p>
+    </div>
+  </div>
+</template>
+
+<script  lang="ts">
+import { PropType } from 'vue';
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    palce: Object as PropType<palceType>
+  }
+})
+export default class CardComp extends Vue {
+  palce!: palceType
+}
+type palceType = {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  boundingbox: string[];
+  lat: string;
+  lon: string;
+  display_name: string;
+  class: string;
+  type: string;
+  importance: number;
+  icon: string;
+  address: Address;
+}
+
+type Address = {
+  hamlet: string;
+  county: string;
+  state: string;
+  'ISO3166-2-lvl4': string;
+  country: string;
+  country_code: string;
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  flex: 1 1 33%;
+  width: 33%;
+  max-width: 33%;
+  padding: 0px 8px;
+  margin-bottom: 16px;
+  border: 3px solid rgb(49, 47, 199);
+  margin: 10px;
+  padding: 10px;
+
+  img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+    transition: 0.4s;
+  }
+
+  .titleWarn {
+    text-align: center;
+  }
+
+  .wrapperText {
+    margin-top: 10px;
+    display: flex;
+    text-align: center;
+
+    p {
+      margin-left: 10px;
+    }
+  }
+
+  h3 {
+    padding: 16px;
+    color: #313131;
+    transition: 0.4s;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.05);
+    }
+
+    h3 {
+      color: #24c444;
+    }
+  }
+}
+</style>
